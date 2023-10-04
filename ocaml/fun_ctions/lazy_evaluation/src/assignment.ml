bd9d4db9e647933280ec23556407b9cdd4568b3e
Andreas Stömmer <andreas.stoemmer@tum.de>
2023-10-04 22:01:36 +0200

    d

--- ocaml/fun_ctions/lazy_evaluation/src/assignment.ml
+++ ocaml/fun_ctions/lazy_evaluation/src/assignment.ml
@@ -1,12 +1,39 @@
-type 'a inf_list = unit (* TODO *)
+type 'a inf_list = Cons of 'a*(unit->'a inf_list)
 
-let integers _ = failwith "TODO"
-let take _ = failwith "TODO"
-let map _ = failwith "TODO"
-let filter _ = failwith "TODO"
+let rec integers n = Cons(n, fun () -> integers @@ (n+1))
+
+(*let nat_nb = integers 0
+let (Cons(zero,_)) = 
+let (Cons(one,_)) = l*)
+
+let take n = 
+  let rec take' n acc = function 
+    |_ -> acc
+    |Cons(a,xs) -> if n > 0 then take' (n-1) (a::[]) xs
+    else acc
+in take' n [] 
+
+let rec take n (Cons(head,tail))= 
+ if n > 0 then head:: (take (n-1) @@ tail ()) else []
+
+let rec map f (Cons(head,tail)) = Cons((f head), fun () -> map f (tail ()))
+
+let rec filter p (Cons(head,tail)) = 
+  if p head then Cons(head, fun () -> filter p (tail ()))
+  else filter p (tail ())
+
+let rec filter p (Cons(head,tail)) = 
+  let lazy_filtered_tail () = filter p @@ tail () in
+    if p head then Cons(head, lazy_filtered_tail)
+    else filter p @@ lazy_filtered_tail ()
+
 
 type 'a lazy_value = unit -> 'a
 
-let map_lazy _ = failwith "TODO"
-let bind_lazy _ = failwith "TODO"
-let combine_lazy _ = failwith "TODO"
+let map_lazy f (a) = fun () -> f ( a ())
+
+let bind_lazy bind a  = fun () -> bind (a())
+let bind_lazy (bind: 'a -> 'b lazy_value) a  = (bind @@ a ()) ()
+
+let combine_lazy l1 l2 = fun () -> (l1 (), l2 ())
+let combine_lazy l1 l2 () = (l1 (), l2 ())
--- ocaml/recursion_and_fold/list-recursive/src/assignment.ml
+++ ocaml/recursion_and_fold/list-recursive/src/assignment.ml
@@ -6,3 +6,57 @@ let map _ _ = failwith "TODO"
 let fold_left _ _ _ = failwith "TODO"
 let filter _ _ = failwith "TODO"
 let partition _ _ = failwith "TODO"
+
+(*tail-recursive*)
+
+let rec length = function [] -> 0 | _ :: xs -> 1 + length xs
+let length list = let rec length' acc = function [] -> acc | _::xs -> length' (acc+1) xs in length' 0 list
+
+let rec sum_int = function [] -> 0 | x :: xs -> x + length xs
+let sum_int = let rec sum_int' acc = function [] -> acc | x::xs -> sum_int' (acc+x) xs in sum_int' 0
+
+let rec sum_float = function [] -> 0. | x :: xs -> x +. sum_float xs
+let sum_float = let rec sum_float' acc = function [] -> acc | x :: xs -> sum_float' (acc+.x) xs in sum_float' 0.
+
+let rec prod_int = function [] -> 1 | x :: xs -> x * prod_int xs
+let prod_int = let rec prod_int' acc = function [] -> acc | x :: xs -> prod_int' (acc*x) xs in prod_int' 1
+
+let rec map f = function [] -> [] | x :: xs -> f x :: map f xs
+let map f l = let rec map' f acc = function [] -> List.rev acc | x :: xs -> map' f ((f x)::acc) xs in map' f [] l
+(*map f l ?*)
+
+let rec fold_left f acc = function
+  | [] -> acc
+  | x :: xs -> fold_left f (f acc x) xs
+let rec fold_left f acc = function
+  | [] -> acc
+  | x :: xs -> fold_left f (f acc x) xs
+
+let rec filter pred = function
+  | [] -> []
+  | x :: xs when pred x -> x :: filter pred xs
+  | _ :: xs -> filter pred xs
+let filter pred l = 
+  let rec filter' pred acc = function
+  | [] -> List.rev acc
+  | x :: xs when pred x -> filter' pred (x::acc) xs
+  | _ :: xs -> filter' pred acc xs
+in filter' pred [] l
+
+let rec partition p = function
+  | [] -> ([], [])
+  | x :: xs ->
+      let yes, no = partition p xs in
+      if p x then (x :: yes, no) else (yes, x :: no)
+let partition p = 
+  let rec partition' p acc1 acc2 = function
+  | [] -> (List.rev acc1, List.rev acc2)
+  | x :: xs -> if p x then partition' p (x::acc1) acc2 xs else partition' p acc1 (x::acc2) xs
+  in partition' p [] [] 
+let partition p = 
+    let rec partition' p acc1 acc2 = function
+    | [] -> (List.rev acc1, List.rev acc2)
+    | x::xs when p x -> partition' p (x::acc1) acc2 xs 
+    | x::xs -> partition' p acc1 (x::acc2) xs
+    in partition' p [] [] 
+
