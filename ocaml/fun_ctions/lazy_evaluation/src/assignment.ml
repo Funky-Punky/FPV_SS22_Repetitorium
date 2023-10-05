754d234020541040c9fc5b29fd138113a4983600
Michael Kuckuk <8076094+LBBO@users.noreply.github.com>
2023-10-05 18:10:55 +0200

    Add notes from day 6

--- ocaml/fun_ctions/lazy_evaluation/src/assignment.ml
+++ ocaml/fun_ctions/lazy_evaluation/src/assignment.ml
@@ -1,12 +1,32 @@
-type 'a inf_list = unit (* TODO *)
+type 'a inf_list = Cons of 'a * (unit -> 'a inf_list)
 
-let integers _ = failwith "TODO"
-let take _ = failwith "TODO"
-let map _ = failwith "TODO"
-let filter _ = failwith "TODO"
+let rec integers n = Cons (n, fun () -> integers (n + 1))
+(* let natural_numbers = integers 0
+   let (Cons (zero, natural_numbers_according_to_mathmaticians)) = natural_numbers
+   let (Cons (one, _)) = natural_numbers_according_to_mathmaticians () *)
+
+let rec take n (Cons (head, tail)) =
+  if n > 0 then head :: (take (n - 1) @@ tail ()) else []
+
+let rec map f (Cons (head, tail)) = Cons (f head, fun () -> map f (tail ()))
+
+(* ⚠️ THIS NEVER TERMINATES ☠️ *)
+(* let rec map f (Cons (head, tail)) =
+   Cons
+     ( f head,
+       let new_tail = map f (tail ()) in
+       fun () -> new_tail ) *)
+
+let rec filter pred (Cons (head, tail)) =
+  let lazy_filtered_tail () = filter pred @@ tail () in
+  if pred head then Cons (head, lazy_filtered_tail)
+  else filter pred @@ lazy_filtered_tail ()
 
 type 'a lazy_value = unit -> 'a
 
-let map_lazy _ = failwith "TODO"
-let bind_lazy _ = failwith "TODO"
-let combine_lazy _ = failwith "TODO"
+(* let immediate_5 : int = 5
+   let lazy_5 : int lazy_value = fun () -> 5 *)
+
+let map_lazy f a () = f (a ())
+let bind_lazy (bind : 'a -> 'b lazy_value) a () = (bind @@ a ()) ()
+let combine_lazy l1 l2 () = (l1 (), l2 ())
